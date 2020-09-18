05e40caf4657d2edb54f91093ffd9fb647f09b4d
Vincent <Vincent.Fermo1@marist.edu>
2020-09-18 14:47:21 -0400

    3rd String Python program.

---
+++ string3.py
@@ -0,0 +1,13 @@
+# Introduction to Coding
+# Assignment 4, Professor Arias
+# September 18, 2020
+
+def string_both_ends(str):
+  if len(str) < 2:
+    return ''
+
+  return str[0:2] + str[-2:]
+
+print(string_both_ends('w3resource'))
+print(string_both_ends('w3'))
+print(string_both_ends('w'))
