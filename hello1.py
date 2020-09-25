399dc537212250a873b8ff9b7e04daf1e4a681af
Juan Arias <juan.arias@marist.edu>
2020-09-25 09:54:23 -0400

    Feedback

--- hello1.py
+++ hello1.py
@@ -3,4 +3,5 @@
 # 9/4/20
 def main ():
     print ("Hello, instructor!")
+    # Here you had to add a second print statement
 main()
--- sphere1.py
+++ sphere1.py
@@ -1,5 +1,7 @@
+# JA: It's cleaner if you put it inside a main() method.
+# You should calculate area and volume in a single program
 pi = 3.14
 r = 6
-v = 4/3*pi* r*3
+v = 4/3*pi* r*3 # r**3
 
 print("The volume of the sphere is: " ,v)
