92ebe04322f2bdf3e69a81ff5a0029347bc0264c
Juan Arias <juan.arias@marist.edu>
2020-11-23 14:36:01 -0500

    Feedback

--- graphic_gates.py
+++ graphic_gates.py
@@ -3,27 +3,28 @@
 # Project 2
 
 from graphics import *
+import random
 
-def draw_not(x, y, size, window):
+def draw_not(x, y, size, win):
     Line(Point(x+size,y),Point(x+size+10,y)).draw(win)
     vertices = []
     for i in range(3):                         
         x = random.randint(0, 500)             
         y = random.randint(0, 500)             
-        vertices.append(graphics.Point(x, y))  
-    triangle = graphics.Polygon(vertices)      
-    triangle.setFill(colour)
+        vertices.append(Point(x, y))  
+    triangle = Polygon(vertices)      
+    triangle.setFill("red")
     triangle.draw(win)
     Line(Point(x1,y1),Point(x1,y2)).draw(win)
 
-def draw_xor(x,y,size,window):
+def draw_xor(x,y,size,win):
     Line(Point(x1-10,y3),Point(x1,y3)).draw(win)
     Line(Point(x1-10,y4),Point(x1,y4)).draw(win)
     Arc(Point(x,y1),Point(x+size,y2)).draw(win)
     Arc(Point(x1,y3),Point(x+size,y3)).draw(win)
     Line(Point(x+size,y),Point(x+size+10,y)).draw(win)
 
-def draw_nand(x,y,size,window):
+def draw_nand(x,y,size,win):
     Line(Point(x1-10,y3),Point(x1,y3)).draw(win)
     Line(Point(x1-10,y4),Point(x1,y4)).draw(win)
     Arc(Point(x,y1),Point(x+size,y2)).draw(win)
@@ -31,9 +32,17 @@ def draw_nand(x,y,size,window):
     c = Circle(Point(50,50), 10)
     c.draw(win)
     win.getMouse()
-    win.close()
+    win.close()
+
     Line(Point(x+size,y),Point(x+size+10,y)).draw(win)
     
-    
-    
+def main():
+    win = GraphWin("Logical Gates",600,600)
+    draw_not(50,50,50,win)
+    draw_xor(50,200,50,win)
+    draw_nand(50,350,50,win)
+    win.getMouse()
+
+if __name__ == "__main__":
+    main()
     
